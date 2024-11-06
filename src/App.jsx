a8c8810bae6bfe09f9a5a99f1b8cb545eae077aa
VinaySehwag14 <vinaysehwag14@gmail.com>
2024-11-06 13:27:57 +0530

    setted up redux toolkit store

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { Footer, Header } from "./components";
+import { login, logout } from "./store/authSlice";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+    return !loading ? (
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>{/* <Outlet/> */}</main>
+          <Footer />
+        </div>
+      </div>
+    ) : (
+      <></>
+    );
+  }, []);
+
   return <div>App</div>;
 };
 
